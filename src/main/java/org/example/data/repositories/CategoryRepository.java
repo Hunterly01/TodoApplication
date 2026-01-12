3ede9ad618a726ae4cd6b157716ad13ac9c69e73
Martin <martinngutswen02@gmail.com>
2026-01-12 09:03:32 +0100

    Update

--- src/main/java/org/example/data/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.data.repositories;
-
-import org.example.data.models.Category;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CategoryRepository extends MongoRepository<Category, String> {
-}
--- src/main/java/org/example/data/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.data.repositories;
-
-import com.mongodb.client.MongoClient;
-import org.example.data.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends MongoRepository<User,Long> {
-}
